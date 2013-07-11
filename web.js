e53a34e7c021b1645ac40fd21b1d7e1ab3836833
Ubuntu <ubuntu@ip-172-31-25-253.us-west-2.compute.internal>
2013-07-11 02:03:24 +0000

    test main entry

--- web.js
+++ web.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var http = require("http");
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(port);
