735823a384c873d614b0e4d9bdb1e06e50424c17
Ubuntu <ubuntu@ip-172-31-25-253.us-west-2.compute.internal>
2013-07-11 02:02:12 +0000

    Added server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+var port = process.env.PORT || 5000;
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(port);
